6abebc7216f4440c16291cd04c63ea20e0300ed0
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2023-05-09 18:57:41 +0100

    chore: student registration stashed (#10)

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const wardenAuth = require('../middlewares/warden-auth');
 
 const wardenRegister = require('../controllers/wardens/register');
 const wardenLogin = require('../controllers/wardens/login');
+const wardenRegistrationCompletion = require('../controllers/wardens/complete-registration');
 
+const studentRegister = require('../controllers/students/register')
 
 
 
 //WARDEN
 router.post('/warden/register', wardenRegister);
 router.post('/warden/login', wardenLogin);
+router.post('/warden/update-reg', wardenAuth, wardenRegistrationCompletion );
+
+
+//STUDENT
+router.post('/student/register', studentRegister);
 
 
 
