685dad31834ceff0b87e894d01e4ef5a7d4ae96d
Leona-cloud <chisomokafor038@gmail.com>
2023-07-21 16:36:53 +0100

    chore: quick fix

--- controllers/students/setup-hostel.js
+++ controllers/students/setup-hostel.js
@@ -10,7 +10,7 @@ const setupHostel = async (req, res) => {
     const student = await Student.findOne({ _id: authenticatedUser.id }).select(['-password'])
     if (!student) return errorResponse(400, res, "Student does not exist");
    
-    if(student.hostelId !== null) return errorResponse(400, res, "Room already exists");
+    if(student.hostelId !== null) return errorResponse(400, res, "Student booked a room already");
 
     //create endpoiont to fetch hostels based on gender
     const { hostelId, block, roomNumber } = req.body;
