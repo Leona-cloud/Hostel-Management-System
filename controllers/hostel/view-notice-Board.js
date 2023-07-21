ec954b00534c04e9321540339a117bf29bbc0d67
Leona-cloud <chisomokafor038@gmail.com>
2023-07-21 16:15:46 +0100

    chore: quick fix

--- controllers/hostel/view-notice-Board.js
+++ controllers/hostel/view-notice-Board.js
@@ -7,13 +7,26 @@ const Student = require('../../models/student');
 
 const viewNoticeBoard = async(req, res)=>{
 
+    const { page } = req.body;
+
+    let pageNumber;
+        if(page === undefined){
+            pageNumber = 0;
+        }else{
+            pageNumber = Number(page) - 1;
+        };
+
+        const pageSize = 3
+    
+        const pagination = pageNumber * pageSize;
+
     const authenticatedUser = req.user
 
     const studentExists = await Student.findOne({_id: authenticatedUser.id})
     if(!studentExists) return res.status(400).json({success: false, message: 'user does not exist'});
 
     try {
-        const fetchNotice = await NoticeBoard.find().sort({createdAt: -1})
+        const fetchNotice = await NoticeBoard.find().limit(pageSize).skip(pagination).sort({createdAt: 1})
         
         console.log(fetchNotice)
         return res.status(200).json({
